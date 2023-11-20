812dc7a28aa64997028f935b8b4139cfb6c362e7
RamNerusu26 <ramnerusu08@gmail.com>
2023-11-20 20:54:40 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ram(request):
+    return render(request,'ram.html')
+def bindu(request):
+    return render(request,'bindu.html')
+def ram1(request):
+    return render(request,'ram1.html')
+def bindu1(request):
+    return render(request,'bindu1.html')
